4fc75c9dfe6f25a11ea07cdbbea806b82a6df08e
email <b1017216@gmail.com>
2019-10-09 16:34:58 +0900

    問題の画面と同じ状況を再現完了

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function aco() {
+    var tmp = document.getElementById("fingermenu").style.visibility;
+    tmp = (tmp == "visible") ? "hidden" : "visible";
+    document.getElementById("fingermenu").style.visibility = tmp;
+}
